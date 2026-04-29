ee4cc41c6c6dbb001686afc77f5d7536118f64bb
MOHD NAWAZ KHAN <khanmohdnawaz567@gmail.com>
2026-04-29 18:19:36 +0530

    fix env path

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./backend/config/config.env" });
 
 const express = require('express');
 const cloudinary = require('cloudinary');
@@ -7,15 +7,12 @@ const connectDatabase = require('./backend/config/database');
 
 const PORT = process.env.PORT || 4000;
 
-
 process.on('uncaughtException', (err) => {
     console.error(`Error: ${err.message}`);
     process.exit(1);
 });
 
-
 connectDatabase();
-
 
 cloudinary.v2.config({
     cloud_name: process.env.CLOUDINARY_NAME,
@@ -23,16 +20,13 @@ cloudinary.v2.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-
 app.get('/', (req, res) => {
     res.send('API is running 🚀');
 });
 
-
 const server = app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
 });
-
 
 process.on('unhandledRejection', (err) => {
     console.error(`Error: ${err.message}`);
